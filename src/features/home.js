11d7a03da129b6db52f4eb1a7d05688e8d615c89
heavenstar627 <dhorse443@gmail.com>
2021-10-11 12:20:45 +0900

    welldone with component

--- src/features/home.js
+++ src/features/home.js
@@ -1,27 +1,8 @@
 import React from "react";
-import Provider from "react-redux/es/components/Provider";
-import TextInput from "@workday/canvas-kit-react-text-input";
-import FormField from "@workday/canvas-kit-react-form-field";
-import { HiOutlineCog } from "react-icons/hi";
 import { FcHome } from "react-icons/fc";
-import { FaAddressCard, FaCentercode } from "react-icons/fa";
-import { IoMdRocket } from "react-icons/io";
-import { MdOutlineCardGiftcard } from "react-icons/md";
-import { GiTakeMyMoney } from "react-icons/gi";
-import { GiPayMoney } from "react-icons/gi";
-import { BsFillBarChartLineFill } from "react-icons/bs";
-import { FaRegAddressBook } from "react-icons/fa";
-import { GiBoltShield } from "react-icons/gi";
-import { SiTimescale } from "react-icons/si";
-import { GiPlayerTime } from "react-icons/gi";
-import { GiTeacher } from "react-icons/gi";
-import { RiSettings4Fill } from "react-icons/ri";
-
-
-
 import { makeStyles } from "@material-ui/core/styles";
+import IconComponent from "./iconComponent";
 import "../../src/App.css";
-import { flex } from "@workday/canvas-kit-react/dist/commonjs/pagination/lib/Pagination/common/utils/flex";
 
 const Home = () => {
     const classes = useStyles();
@@ -32,10 +13,10 @@ const Home = () => {
                     <p className="cmxLogo">CMx</p>
                 </div>
                 <div className="title">
-                    <h1 className={classes.titleTop}>
+                    <h1 className={classes.titleTop} id="titleTop">
                         Choose your application
                     </h1>
-                    <p className={classes.titleWelcome}>Welcome, Han Solo</p>
+                    <p className={classes.titleWelcome} id="titleWelcome">Welcome, Han Solo</p>
                 </div>
                 <div className="application">
                     <div className="wrapApp">
@@ -54,127 +35,18 @@ const Home = () => {
                             </div>
                         </div>
                         <div className="icons">
-                            <div className="eachIcon">
-                                <a
-                                    href="https://live.contractexperience.com/login?execution=e1s1"
-                                    className="link linkSpecial"
-                                >
-                                    <div className="personal">
-                                        <FaAddressCard className="personalIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">
-                                        Contract
-                                        <br />
-                                        management
-                                    </div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a
-                                    href="https://design.workday.com/"
-                                    className="link"
-                                >
-                                    <div className="workday">
-                                        <IoMdRocket className="workdayIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">
-                                        Workday Site
-                                    </div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                  <div className="purchases">
-                                        <MdOutlineCardGiftcard className="purchaseIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">Purchase</div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                    <div className="expenses">
-                                        <GiTakeMyMoney className="expenseIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">Expenses</div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                    <div className="pay">
-                                        <GiPayMoney className="payIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">Pay</div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                    <div className="performance">
-                                        <BsFillBarChartLineFill className="performanceIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">Performance</div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                    <div className="directory">
-                                        <FaRegAddressBook className="directoryIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">
-                                        Directory
-                                    </div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                    <div className="benefits">
-                                        <GiBoltShield className="benefitIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">Benefits</div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                    <div className="timeOff">
-                                        <SiTimescale className="timeoffIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">Time Off</div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                    <div className="time">
-                                        <GiPlayerTime className="timeIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">Time</div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                    <div className="teaching">
-                                        <GiTeacher className="teachingIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">Teaching</div>
-                                </a>
-                            </div>
-
-                            <div className="eachIcon">
-                                <a href="#" className="link">
-                                    <div className="setting">
-                                        <RiSettings4Fill className="teachingIcon iconApp" />
-                                    </div>
-                                    <div className="underLetter">Setting</div>
-                                </a>
-                            </div>
+                          <IconComponent url="https://live.contractexperience.com/login?execution=e1s1" underLetter="Contract" underLetterSpecial="management" reactIcon="FaAddressCard" />
+                          <IconComponent url="https://design.workday.com/" underLetter="Workday Site" reactIcon="IoMdRocket" />
+                          <IconComponent url="#" underLetter="Purchase" reactIcon="MdOutlineCardGiftcard" />
+                          <IconComponent url="#" underLetter="Expenses" reactIcon="GiTakeMyMoney" />
+                          <IconComponent url="#" underLetter="Pay" reactIcon="GiPayMoney" />
+                          <IconComponent url="#" underLetter="Performance" reactIcon="BsFillBarChartLineFill" />
+                          <IconComponent url="#" underLetter="Directory" reactIcon="FaRegAddressBook" />
+                          <IconComponent url="#" underLetter="Benefits" reactIcon="GiBoltShield" />
+                          <IconComponent url="#" underLetter="Time Off" reactIcon="SiTimescale" />
+                          <IconComponent url="#" underLetter="Time" reactIcon="GiPlayerTime" />
+                          <IconComponent url="#" underLetter="Teaching" reactIcon="GiTeacher" />
+                          <IconComponent url="#" underLetter="Setting" reactIcon="RiSettings4Fill" />
                         </div>
                     </div>
                 </div>
@@ -191,6 +63,7 @@ const useStyles = makeStyles((theme) => ({
         fontWeight: theme.palette.fontWeight.large,
         display: "flex",
         justifyContent: "center",
+        textAlign: "center"
     },
     titleWelcome: {
         fontSize: theme.palette.fontSizes.medium,
--- src/features/iconComponent.js
+++ src/features/iconComponent.js
@@ -1,32 +1,64 @@
 import React from "react";
-import { FcHome } from "react-icons/fc";
-import { FaAddressCard, FaCentercode } from "react-icons/fa";
+import { FaAddressCard} from "react-icons/fa";
 import { IoMdRocket } from "react-icons/io";
 import { MdOutlineCardGiftcard } from "react-icons/md";
-import { GiTakeMyMoney } from "react-icons/gi";
-import { GiPayMoney } from "react-icons/gi";
+import { GiTakeMyMoney,GiPayMoney, GiBoltShield, GiPlayerTime, GiTeacher} from "react-icons/gi";
 import { BsFillBarChartLineFill } from "react-icons/bs";
 import { FaRegAddressBook } from "react-icons/fa";
-import { GiBoltShield } from "react-icons/gi";
 import { SiTimescale } from "react-icons/si";
-import { GiPlayerTime } from "react-icons/gi";
-import { GiTeacher } from "react-icons/gi";
 import { RiSettings4Fill } from "react-icons/ri";
 
 const IconComponent = (props) => {
+    const Letter = props.underLetter;
+    let LetterWriting;
+    if (Letter==="Contract") {
+        LetterWriting = 
+        <div>
+             <div className="underLetter">{props.underLetter}</div>
+             <div className="underLetter">{props.underLetterSpecial}</div>
+        </div>
+    } else {
+        LetterWriting =
+        <div className="underLetter">{props.underLetter}</div>
+    }
     return (
         <div className="eachIcon">
             <a href={props.url} className="link">
-                <div className="iconApp">
-                    {props.reactIcon}
+                <div>
+                    {(() => {
+                        switch (props.reactIcon) {
+                            case "FaAddressCard":
+                                return <FaAddressCard className="iconApp" />;
+                            case "IoMdRocket":
+                                return <IoMdRocket className="iconApp" />;
+                            case "MdOutlineCardGiftcard":
+                                return <MdOutlineCardGiftcard className="iconApp" />;
+                            case "GiTakeMyMoney":
+                                return <GiTakeMyMoney className="iconApp" />;
+                            case "GiPayMoney":
+                                return <GiPayMoney className="iconApp" />;
+                            case "BsFillBarChartLineFill":
+                                return <BsFillBarChartLineFill className="iconApp" />;
+                            case "FaRegAddressBook":
+                                return <FaRegAddressBook className="iconApp" />;
+                            case "GiBoltShield":
+                                return <GiBoltShield className="iconApp" />;
+                            case "SiTimescale":
+                                return <SiTimescale className="iconApp" />;
+                            case "GiPlayerTime":
+                                return <GiPlayerTime className="iconApp" />;
+                            case "GiTeacher":
+                                return <GiTeacher className="iconApp" />;
+                            case "RiSettings4Fill":
+                                return <RiSettings4Fill className="iconApp" />;
+                            default:
+                                return null;
+                        }
+                    })()}
                 </div>
-                <div className="underLetter">
-                    {props.underLetter}
-                </div>
+                {LetterWriting}
             </a>
         </div>
     );
-}
+};
 export default IconComponent;
-
-<IconComponent url="#" underLetter="workday" reactIcon="MdOutlineCardGiftcard" />
