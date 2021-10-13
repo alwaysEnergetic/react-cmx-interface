ea4e601ecca061cfa588314555f93361cfdc139f
heavenstar627 <dhorse443@gmail.com>
2021-10-13 11:51:18 +0900

    with login responsive

--- src/features/login.js
+++ src/features/login.js
@@ -5,56 +5,59 @@ import FormField from '@workday/canvas-kit-react-form-field';
 import {Button} from '@workday/canvas-kit-react-button';
 import { FaUser, FaLock } from "react-icons/fa";
 import { FiArrowRightCircle } from "react-icons/fi";
-
-
+import logo from './logo.png';
+import background from "./background.jpg"
 import "../../src/App.css";
 
 const Login = () => {
     const classes = useStyles();
     return (
-        <div className="loginPage">
-            <div className="loginTop">
-                <div className="loginLogo">
-                    <div id="cmx">
-                        CMx
-                    </div>
-                    <div id="description">
-                        Expert partner in your growth
-                    </div>
-                </div>
-                <div className="loginTitle">
-                    Contract Management System
-                </div>
-            </div>
-            <div className="loginContent">
-                <div>
-                    <div className="loginInfoMessage">
-                        Please Enter Your Information
-                    </div>
+        <div className="loginPage canvas" style={{ backgroundImage: `url(${background})`}} >
+            <div className="loginContent canvasBackground">
+                <div className="loginDesc">
                     <div>
-                        <div className="email" >
-                            <FormField className="form-group position-relative" inputId="my-input-field">
-                            <TextInput className="form-control input-lg rounded-lg inputUserName" placeholder="Email:" />
-                            <FaUser className="position-absolute customize" />
-                            </FormField>
+                        <div>
+                            <img src={logo}  alt="logo" className="imgSrc" />
                         </div>
-                        <div className="password" >
-                            <FormField className="form-group position-relative" inputId="my-input-field">
-                            <TextInput className="form-control input-lg rounded-lg inputPassword" placeholder="Password:" />
-                            <FaLock className="position-absolute customize" />
-                            </FormField>
+                        <div className="letter">
+                            Expert Partner in your growth
                         </div>
-                        <div className="loginButton">
-                            <div className="w-full">
-                                <Button variant={Button.Variant.Primary} className="secondaryButton">
-                                    <div className="flex">
-                                        Login
-                                        <FiArrowRightCircle className="key"/>
-                                    </div>
-
-                                </Button>
+                    </div>
+                </div>
+                <div className="loginFo">
+                    <div className="loginForm">
+                        <div className="signIn">
+                            Sign in
+                        </div>
+                        <div className="loginInfoMessage">
+                            Please Enter Your Information
+                        </div>
+                        <div className="loginTwo">
+                            <div className="email" >
+                                <FormField className="form-group position-relative" inputId="my-input-field">
+                                    <TextInput className="form-control input-lg rounded-lg inputUserName inputVal " placeholder="Email:" />
+                                    <FaUser className="position-absolute customize" />
+                                </FormField>
+                                
                             </div>
-                            
+                            <div className="password" >
+                                <FormField className="form-group position-relative" inputId="my-input-field">
+                                <TextInput className="form-control input-lg rounded-lg inputPassword inputVal" placeholder="Password:" />
+                                <FaLock className="position-absolute customize" />
+                                </FormField>
+                            </div>
+                            <div className="loginButton">
+                                <div className="w-full">
+                                    <Button variant={Button.Variant.Primary} className="secondaryButton">
+                                        <div className="flex">
+                                            Login
+                                            <FiArrowRightCircle className="key"/>
+                                        </div>
+
+                                    </Button>
+                                </div>
+                                
+                            </div>
                         </div>
                     </div>
                 </div>    
